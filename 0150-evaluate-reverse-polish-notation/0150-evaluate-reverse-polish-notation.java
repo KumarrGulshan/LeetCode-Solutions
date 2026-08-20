456219f1b93472e9e932016e46c0d63bf7971ec6
Gulshan Kumar <157052955+KumarrGulshan@users.noreply.github.com>
2026-08-20 19:49:01 +0530

    Time: 6 ms (87.26%), Space: 45.2 MB (63.18%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -13,14 +13,12 @@ class Solution {
                 case "+":
                     stack.push(stack.pop() + stack.pop());
                     break;
-
                 case "-": {
                     int b = stack.pop();
                     int a = stack.pop();
                     stack.push(a - b);
                     break;
                 }
-
                 case "*":
                     stack.push(stack.pop() * stack.pop());
                     break;
@@ -31,12 +29,10 @@ class Solution {
                     stack.push(a / b);
                     break;
                 }
-
                 default:
                     stack.push(Integer.parseInt(token));
             }
         }
-
         return stack.pop();
     }
 }
