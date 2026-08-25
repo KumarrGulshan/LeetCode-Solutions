2bb415dc51f262fc6555c961a4c4c658999d3216
Gulshan Kumar <157052955+KumarrGulshan@users.noreply.github.com>
2026-08-25 18:00:32 +0530

    Time: 12 ms (34.74%), Space: 46.3 MB (86.38%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,55 +1,44 @@
 import java.util.Stack;
 
-class Solution {
-
-    public int calculate(String s) {
-
-        Stack<Integer> stack = new Stack<>();
-
+class Solution{
+    public int calculate(String s){
+        int num = 0;
         int result = 0;
-        int number = 0;
         int sign = 1;
 
-        for (int i = 0; i < s.length(); i++) {
-
-            char ch = s.charAt(i);
-
-            if (Character.isDigit(ch)) {
-
-                number = number * 10 + (ch - '0');
-
-            } else if (ch == '+') {
-
-                result += sign * number;
-                number = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i<s.length(); i++){
+            int ch = s.charAt(i);
+            if(Character.isDigit(ch)){
+                num = num*10 + (ch -'0');
+            }
+            else if(ch =='+'){
+                result += sign*num;
                 sign = 1;
-
-            } else if (ch == '-') {
-
-                result += sign * number;
-                number = 0;
+                num = 0;
+            }
+            else if(ch=='-'){
+                result += sign*num;
                 sign = -1;
-
-            } else if (ch == '(') {
-
+                num =0;
+            }
+            else if(ch =='('){
                 stack.push(result);
                 stack.push(sign);
-
-                result = 0;
-                sign = 1;
-
-            } else if (ch == ')') {
-
-                result += sign * number;
-                number = 0;
-
-                result *= stack.pop();   // sign
-                result += stack.pop();   // previous result
+                sign =1;
+                result =0;
+            }
+            else if (ch == ')') {
+                result += sign * num;
+                num = 0;
+                int previousSign = stack.pop();
+                int previousResult = stack.pop();
+                result = previousResult + previousSign * result;
             }
         }
-
-        result += sign * number;
+        result += sign * num;
 
         return result;
     }
+
 }
